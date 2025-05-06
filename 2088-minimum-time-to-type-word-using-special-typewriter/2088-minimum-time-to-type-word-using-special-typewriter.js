2e8854265e0258353e1e5aadc0a21ff4a3dda887
wave2 <35678491+songgoeun@users.noreply.github.com>
2025-05-06 17:42:05 +0900

    Time: 1 ms (57.06%), Space: 53.9 MB (52.94%) - LeetHub

---
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minTimeToType = function(word) {
+    let ops = 0;
+    let cur = 'a';
+
+    for(const char of word) {
+        const diff = Math.abs(cur.charCodeAt(0) - char.charCodeAt(0));
+        if(diff > 13) {
+            ops += 26 - diff+1;
+        } else {
+            ops += diff + 1;
+        }
+
+        cur = char;
+    }
+
+    return ops;
+};
