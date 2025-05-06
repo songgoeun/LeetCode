0af760a6550f92fd5d68336f46ec3a6564da2c68
wave2 <35678491+songgoeun@users.noreply.github.com>
2025-05-06 16:58:09 +0900

    Time: 1 ms (74.9%), Space: 57.5 MB (89.96%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.js
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var buildArray = function(nums) {
-    let result = [];
-    for(let i=0; i<nums.length; i++) {
-        const a = nums[i];
-        const b = nums[a];
-        result.push(b);
-    }
-    return result;
+    return nums.map((i) => nums[i]);
 };
