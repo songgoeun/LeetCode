4d9aa60f29e0d6e306b1d6e9c036d12690451b6d
wave2 <35678491+songgoeun@users.noreply.github.com>
2025-05-11 16:22:48 +0900

    Time: 0 ms (100%), Space: 55.5 MB (13.53%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    let count = 0;
+    for(let i=0; i<arr.length; i++) {
+        if(count >= 3) {
+            break;
+        }
+        
+        if(arr[i] % 2 !== 0) {
+            count += 1;
+        } else {
+            count = 0;
+        }
+    }
+
+    return count >= 3;
+};
